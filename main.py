0f26c1c35760e9c2066728c48a0dd3f283eea0c4
NakulD-chetan <66295258+NakulD-chetan@users.noreply.github.com>
2024-05-23 00:55:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,28 +17,28 @@ def check_scheduled_task(file_path, config):
     to = config['Twilio']['to_phone']
     from_ = config['Twilio']['from_phone']
     # Get current day and time
-
-    # Read Excel file for the current day's sheet
-    df = pd.read_excel(file_path, usecols=columns, sheet_name=current_day, index_col=index)
-
-    # Check if there are any tasks scheduled for the current time
-    if 'CALL_TIME' in df.columns:
-        column_list = df['CALL_TIME'].tolist()
-        CALL_TIME_LIST = [time_obj.strftime('%H:%M') for time_obj in column_list]
-        while True:
-            current_time=datetime.now().strftime('%H:%M')
-            current_day = datetime.today().strftime('%A').upper()
-            if current_time in CALL_TIME_LIST:
-                logger.info(f"There Exist  Task for {current_time}")
-                filter_df = df.iloc[CALL_TIME_LIST.index(current_time)]
-                Message = filter_df['MESSAGE']
-                calling_on_phone(account_sid=account_sid, auth_token=auth_token, Message=Message, to=to, from_=from_)
-
-            else:
-                logger.info(
-                    f"No task scheduled for the this time,and day-{current_time, current_day}")  # No task scheduled for the current time
-
-            time.sleep(60)
+    while True:
+        current_time=datetime.now().strftime('%H:%M')
+        current_day = datetime.today().strftime('%A').upper()
+        # Read Excel file for the current day's sheet
+        df = pd.read_excel(file_path, usecols=columns, sheet_name=current_day, index_col=index)
+    
+        # Check if there are any tasks scheduled for the current time
+        if 'CALL_TIME' in df.columns:
+            column_list = df['CALL_TIME'].tolist()
+            CALL_TIME_LIST = [time_obj.strftime('%H:%M') for time_obj in column_list]
+            
+                
+                if current_time in CALL_TIME_LIST:
+                    logger.info(f"There Exist  Task for {current_time}")
+                    filter_df = df.iloc[CALL_TIME_LIST.index(current_time)]
+                    Message = filter_df['MESSAGE']
+                    calling_on_phone(account_sid=account_sid, auth_token=auth_token, Message=Message, to=to, from_=from_)
+    
+                else:
+                    logger.info(
+                        f"No task scheduled for the this time,and day-{current_time, current_day}")  # No task scheduled for the current time
+         time.sleep(60)
 
 
 def calling_on_phone(Message, account_sid, auth_token, to, from_):
